ac1c7607dc3fa682028a9030e87b0120f36ef639
paulmolloy <molloyp1@tcd.ie>
2021-03-02 02:55:36 +0100

    feat: example of protoc-gen plugin generated code

---
+++ internal/staticTestFiles/test_case_steering_command_example.messageintegrity.go
@@ -0,0 +1,19 @@
+package verification
+
+import (
+	"github.com/einride/protoc-gen-messageintegrity/internal/verification"
+	"os"
+)
+
+const ImplicitMessageIntegrityKey = "IMPLICIT_MESSAGE_INTEGRITY_KEY"
+
+func (x *SteeringCommandVerification) Sign() error {
+	key := os.Getenv(ImplicitMessageIntegrityKey)
+	return verification.SignProto(x, []byte(key))
+}
+
+func (x *SteeringCommandVerification) Verify() (bool, error) {
+	key := os.Getenv(ImplicitMessageIntegrityKey)
+	return verification.ValidateHMAC(x, []byte(key))
+}
+
