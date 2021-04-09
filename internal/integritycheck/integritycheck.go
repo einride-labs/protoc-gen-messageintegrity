fcac16f024c84fa4ad65a3f3f0840bc64f9a7dba
paulmolloy <molloyp1@tcd.ie>
2021-04-09 21:23:07 +0200

    feat: add check for messageintegrity

--- internal/integritycheck/integritycheck.go
+++ internal/integritycheck/integritycheck.go
@@ -3,15 +3,39 @@ package integritycheck
 
 import (
 	"errors"
+	"fmt"
+	"go/ast"
+	"go/types"
 	"golang.org/x/tools/go/analysis"
+	"google.golang.org/protobuf/proto"
 )
 
 var Analyzer = &analysis.Analyzer{
 	Name: "integritylint",
 	Doc:  "checks signatures are used if message integrity is enabled for protomessages",
 	Run:  run,
+
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
+	for _, file := range pass.Files {
+			ast.Inspect(file, func(n ast.Node) bool {
+			de, ok := n.(*ast.ValueSpec)
+			if !ok {
+				return true
+			}
+
+			fmt.Printf("type: %v, name: %v \n", de.Type ,de.Names)
+
+			msg, ok := de.Type.(*proto.Message)
+			prev := make(map[Type]int)
+			types.lookupType(prev, de.Type)
+			types.Look
+			if !ok {
+				return true
+			}
+			return true
+		})
+	}
 	return nil, errors.New("not implemented yet")
 }
