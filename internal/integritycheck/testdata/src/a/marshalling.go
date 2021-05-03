4998aea1eaf0c271083d50490c55e046d0313e6f
paulmolloy <molloyp1@tcd.ie>
2021-05-03 17:06:24 +0200

    feat: integrity linter minimal working version
    Check for keep track of calls to Marshal() and Unmarshal() on integrity
    option REQUIRED  proto messages.
    Check that Sign is called before Marshals() on instances of these messages.
    Check that Verify is called after Unmarshals() on insances of these messages.
    Report diagnostic on Marshalls() and Unmarshals() that don't satisfy this.
    Add Extra examples to tests to show functionality.

--- internal/integritycheck/testdata/src/a/marshalling.go
+++ internal/integritycheck/testdata/src/a/marshalling.go
@@ -8,15 +8,68 @@ import (
 )
 
 func main() {
-	integrityVerificationExample()
-}
-
-func integrityVerificationExample() {
 	key := "a key for signing"
 	os.Setenv(integpb.ImplicitMessageIntegrityKey, key)
+	signedExample()
+	notSignedExample()
+	VerificationExample()
+	noVerificationExample()
+	notSignedUntilAfterMarshalExample()
+	noIntegrityOptionExample()
+	// TODO(paulmolloy): These cases are not currently handled by the linter.
+	// signedButLaterModifiedExample()
+	// accessBeforeVerificationExample()
+}
 
+func signedExample() {
+	sigSteeringCommand := integpb.SteeringCommandVerificationOption{SteeringAngle: 5.0} // want sigSteeringCommand: `found marshal of message integrity enabled proto message`
+
+	// Sending
+	if err := sigSteeringCommand.Sign(); err != nil {
+		log.Fatalf("failed to sign proto: %v", err)
+	}
+	_, err := proto.Marshal(&sigSteeringCommand)
+	if err != nil {
+		log.Fatalf("failed to marshal verified message: %v ", err)
+	}
+}
+
+func notSignedExample() {
+	sigSteeringCommandUnsigned := integpb.SteeringCommandVerificationOption{SteeringAngle: 5.0} // want sigSteeringCommandUnsigned: `found marshal of message integrity enabled proto message`
+	_, err := proto.Marshal(&sigSteeringCommandUnsigned)                                        // want `found possible marshalling of integrity proto before signing`
+	if err != nil {
+		log.Fatalf("failed to marshal verified message: %v ", err)
+	}
+}
+
+func notSignedUntilAfterMarshalExample() {
+	sigSteeringCommandSignedLate := integpb.SteeringCommandVerificationOption{SteeringAngle: 5.0} // want sigSteeringCommandSignedLate: `found marshal of message integrity enabled proto message`
+	_, err := proto.Marshal(&sigSteeringCommandSignedLate)                                        // want `found possible marshalling of integrity proto before signing`
+	if err != nil {
+		log.Fatalf("failed to marshal verified message: %v ", err)
+	}
+	sigSteeringCommandSignedLate.Sign()
+}
+
+/*
+func signedButLaterModifiedExample() {
+	sigSteeringCommand := integpb.SteeringCommandVerificationOption{SteeringAngle: 5.0} // want sigSteeringCommand: `found marshal of message integrity enabled proto message`
+
+	// Sending
+	if err := sigSteeringCommand.Sign(); err != nil {
+		log.Fatalf("failed to sign proto: %v", err)
+	}
+	sigSteeringCommand = integpb.SteeringCommandVerificationOption{SteeringAngle: 5.0}
+	_, err := proto.Marshal(&sigSteeringCommand) // want `found possible marshalling of integrity proto before signing`
+	if err != nil {
+		log.Fatalf("failed to marshal verified message: %v ", err)
+	}
+}
+*/
+
+func VerificationExample() {
 	var isValid bool
-	sigSteeringCommand := integpb.SteeringCommandVerificationOption{SteeringAngle: 5.0}
+	sigSteeringCommand := integpb.SteeringCommandVerificationOption{SteeringAngle: 5.0} // want sigSteeringCommand: `found marshal of message integrity enabled proto message`
 
 	// Sending
 	if err := sigSteeringCommand.Sign(); err != nil {
@@ -28,7 +81,7 @@ func integrityVerificationExample() {
 	}
 
 	// Receiving
-	var receivedMessage integpb.SteeringCommandVerificationOption
+	var receivedMessage integpb.SteeringCommandVerificationOption // want receivedMessage: `found unmarshal of message integrity enabled proto message`
 	if err = proto.Unmarshal(data, &receivedMessage); err != nil {
 		log.Fatal(err)
 	}
@@ -38,13 +91,64 @@ func integrityVerificationExample() {
 	if !isValid || err != nil {
 		log.Fatalf("failed to verify proto: %v", err)
 	}
+}
+
+func noVerificationExample() {
+	sigSteeringCommand := integpb.SteeringCommandVerificationOption{SteeringAngle: 5.0} // want sigSteeringCommand: `found marshal of message integrity enabled proto message`
+
+	// Sending
+	if err := sigSteeringCommand.Sign(); err != nil {
+		log.Fatalf("failed to sign proto: %v", err)
+	}
+	data, err := proto.Marshal(&sigSteeringCommand)
+	if err != nil {
+		log.Fatalf("failed to marshal verified message: %v ", err)
+	}
+
+	// Receiving
+	var receivedMessage integpb.SteeringCommandVerificationOption  // want receivedMessage: `found unmarshal of message integrity enabled proto message`
+	if err = proto.Unmarshal(data, &receivedMessage); err != nil { // want `found possible unmarshalling of integrity proto without verifying afterwards`
+		log.Fatal(err)
+	}
+}
+
+/*
+func accessBeforeVerificationExample() {
+	var isValid bool
+	sigSteeringCommand := integpb.SteeringCommandVerificationOption{SteeringAngle: 5.0} // want sigSteeringCommand: `found marshal of message integrity enabled proto message`
+
+	// Sending
+	if err := sigSteeringCommand.Sign(); err != nil {
+		log.Fatalf("failed to sign proto: %v", err)
+	}
+	data, err := proto.Marshal(&sigSteeringCommand)
+	if err != nil {
+		log.Fatalf("failed to marshal verified message: %v ", err)
+	}
+
+	// Receiving
+	var receivedMessage integpb.SteeringCommandVerificationOption  // want receivedMessage: `found unmarshal of message integrity enabled proto message`
+	if err = proto.Unmarshal(data, &receivedMessage); err != nil { // want `found possible access or overwrite of unmarshalled of integrity proto without verifying before`
+		log.Fatal(err)
+	}
+	receivedMessage = integpb.SteeringCommandVerificationOption{}
+	isValid, err = receivedMessage.Verify()
+
+	if !isValid || err != nil {
+		log.Fatalf("failed to verify proto: %v", err)
+	}
+}
+*/
+
+func noIntegrityOptionExample() {
+	var isValid bool
 	sigSteeringCommandTwo := integpb.SteeringCommandVerification{SteeringAngle: 5.0}
 
 	// Sending
 	if err := sigSteeringCommandTwo.Sign(); err != nil {
 		log.Fatalf("failed to sign proto: %v", err)
 	}
-	data, err = proto.Marshal(&sigSteeringCommandTwo)
+	data, err := proto.Marshal(&sigSteeringCommandTwo)
 	if err != nil {
 		log.Fatalf("failed to marshal verified message: %v ", err)
 	}
@@ -56,7 +160,6 @@ func integrityVerificationExample() {
 	}
 
 	isValid, err = receivedMessageTwo.Verify()
-
 	if !isValid || err != nil {
 		log.Fatalf("failed to verify proto: %v", err)
 	}
--- internal/integritycheck/util.go
+++ internal/integritycheck/util.go
@@ -4,6 +4,9 @@ import (
 	"errors"
 	"go/ast"
 	"strconv"
+	"go/token"
+	"log"
+	"strings"
 )
 
 // reads a constant byte array in a ast.Composite lit and turns it into a runtime byte array.
@@ -46,4 +49,74 @@ func getIdent(s ast.Expr) *ast.Ident {
 	default:
 		return nil
 	}
+}
+
+func getImportPath(n ast.Node) (*ast.ImportSpec, string, bool) {
+	im, ok := n.(*ast.ImportSpec)
+	if !ok {
+		return nil, "", false
+	}
+	path, err := strconv.Unquote(im.Path.Value)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if strings.Compare(path, "command-line-arguments") == 0 {
+		// I don't know why this always appears as an import but I'm ignoring it.
+		return im, "", false
+	}
+	return im, path, true
+}
+
+func castSelector(n ast.Node) (*ast.SelectorExpr, []ast.Expr, bool) {
+	e, ok := n.(ast.Expr)
+	if !ok {
+		return nil, nil, false
+	}
+	callExpr, ok := e.(*ast.CallExpr)
+	if !ok {
+		return nil, nil, false
+	}
+	sel, ok := callExpr.Fun.(*ast.SelectorExpr)
+	if !ok {
+		return nil, nil, false
+	}
+	return sel, callExpr.Args, true
+}
+
+func castAddrOp(e ast.Expr) (*ast.Ident, bool) {
+	addrExpr, ok := e.(*ast.UnaryExpr)
+	if !ok {
+		return nil, false
+	}
+	if addrExpr.Op != token.AND {
+		return nil, false
+	}
+	ident, ok := addrExpr.X.(*ast.Ident)
+	if !ok {
+		return nil, false
+	}
+	return ident, true
+}
+
+func castProtoRawDesc(o *ast.Object) (ast.Expr, bool) {
+	if o.Kind != ast.Var {
+		return nil, false
+	}
+	// This really should be "file_example_" + <import_file_name> + "_.*_proto_rawDesc"
+	// but I don't know how to get the file name of the proto file name
+	if !strings.Contains(o.Name, "_proto_rawDesc") {
+		return nil, false
+	}
+	if o.Decl == nil {
+		return nil, false
+	}
+	as, ok := o.Decl.(*ast.ValueSpec)
+	if !ok {
+		// Not an assignment statement")
+		return nil, false
+	}
+	if len(as.Values) != 1 {
+		return nil, false
+	}
+	return as.Values[0], true
 }
