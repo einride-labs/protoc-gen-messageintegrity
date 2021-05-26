a366945f0f09d31350accb43ad5cbdd15ba345e2
paulmolloy <molloyp1@tcd.ie>
2021-05-26 16:19:18 +0200

    feat: add rsa key reading based on key id

--- internal/verificationRSAOption/verification.go
+++ internal/verificationRSAOption/verification.go
@@ -1,9 +1,12 @@
-package verificationoption
+package verificationoptionrsa
 
 import (
 	"bytes"
 	"crypto/hmac"
+	"crypto/rsa"
 	"crypto/sha256"
+	"crypto/x509"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	integritypb "github.com/einride/protoc-gen-messageintegrity/proto/gen/integrity/v1"
@@ -11,10 +14,15 @@ import (
 	"google.golang.org/protobuf/reflect/protoreflect"
 	descriptorpb "google.golang.org/protobuf/types/descriptorpb"
 	"hash"
+	"io/ioutil"
 	"log"
+	"os"
+	"path"
 )
 
 const ImplicitMessageIntegrityKey = "IMPLICIT_MESSAGE_INTEGRITY_KEY"
+const ImplicitMessageIntegrityPublicKey = "IMPLICIT_MESSAGE_INTEGRITY_PUBLIC_KEY"
+const ImplicitMessageIntegrityPrivateKey = "IMPLICIT_MESSAGE_INTEGRITY_PRIVATE_KEY"
 
 // VerifiableMessage a proto message that has a Signature field.
 // Using the extension interface pattern so that I can read the Signature.
@@ -22,6 +30,60 @@ type VerifiableMessage interface {
 	proto.Message
 	GetSignature() []byte
 	// Can't modify the Signature field since there is no Setters for proto-gen-go fields using protoreflect instead.
+}
+
+func ValidatePKCS1v15(message VerifiableMessage, key []byte) (bool, error) {
+	if message == nil {
+		return false, errors.New("message was nil")
+	}
+	if key == nil {
+		return false, errors.New("key was nil")
+	}
+	signatureFieldDescriptor, err := retrieveSignatureFieldDescriptor(message)
+	if err != nil {
+		return false, err
+	}
+	receivedSig := message.ProtoReflect().Get(signatureFieldDescriptor).Bytes()
+	if receivedSig == nil {
+		return false, errors.New("signature behaviour required but signature not set")
+	}
+	mac := hmac.New(sha256.New, key)
+	// Calculate the expected signature
+	sig, err := calculateSignature(message, mac)
+	if err != nil {
+		return false, err
+	}
+	return bytes.Equal(receivedSig, sig), nil
+}
+
+// SignProto signs a proto that has a proto field Signature.
+func SignPKCS1v15(message VerifiableMessage, key []byte) error {
+	if message == nil {
+		return errors.New("message was nil")
+	}
+	if key == nil {
+		return errors.New("key was nil")
+	}
+
+	// Using proto reflection to make this work as the field is not otherwise accessible without casting to
+	// a specific type.
+	signatureFieldDescriptor, err := retrieveSignatureFieldDescriptor(message)
+	if err != nil {
+		return err
+	}
+
+	if message.ProtoReflect().Get(signatureFieldDescriptor).Bytes() != nil {
+		log.Printf("Signature for %v has already been set to re-signing...", signatureFieldDescriptor.FullName())
+	}
+	mac := hmac.New(sha256.New, key)
+	sig, err := calculateSignature(message, mac)
+	if err != nil {
+		return err
+	}
+	// Assign the generated signature to the message using reflection as it is a VerifiableMessage and not cast to
+	// its exact proto type.
+	message.ProtoReflect().Set(signatureFieldDescriptor, protoreflect.ValueOfBytes(sig))
+	return nil
 }
 
 // Check the signature embedded in the protobuf message is correct for the message by recalculating
@@ -124,4 +186,52 @@ func calculateSignature(message VerifiableMessage, mac hash.Hash) ([]byte, error
 	}
 	// Return the generated signature.
 	return mac.Sum(nil), nil
+}
+
+func FetchPrivateKey(keyID string) (*rsa.PrivateKey, error) {
+	fileName := fmt.Sprintf("message_integrity_%v_private.pem", keyID)
+	keyBlock, err := FetchKeyBlock(fileName)
+	if err != nil {
+		return nil, err
+	}
+	parsedKey, err := x509.ParsePKCS1PrivateKey(keyBlock.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	return parsedKey, err
+}
+
+func FetchPublicKey(keyID string) (*rsa.PublicKey, error) {
+	fileName := fmt.Sprintf("message_integrity_%v_public.pem", keyID)
+	keyBlock, err := FetchKeyBlock(fileName)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(keyBlock.Type)
+
+	fmt.Println(keyBlock.Bytes)
+	parsedKey, err := x509.ParsePKIXPublicKey(keyBlock.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	parsedRSAKey, ok := parsedKey.(*rsa.PublicKey)
+	if !ok {
+		return nil, errors.New("parsed public key was not RSA, others not supported")
+	}
+	return parsedRSAKey, err
+}
+
+func FetchKeyBlock(fileName string) (*pem.Block, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+	keysPath := path.Join(home, "integrity-keys")
+
+	key, err := ioutil.ReadFile(path.Join(keysPath, fileName))
+	if err != nil {
+		return nil, err
+	}
+	keyBlock, _ := pem.Decode(key)
+	return keyBlock, nil
 }
--- internal/verificationRSAOptionTest/verification_test.go
+++ internal/verificationRSAOptionTest/verification_test.go
@@ -1,12 +1,160 @@
-package verificationoptionTest
+package verificationoptionirsaTest
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
 	verificationoption "github.com/einride/protoc-gen-messageintegrity/internal/verificationOption"
+	verificationoptionrsa "github.com/einride/protoc-gen-messageintegrity/internal/verificationRSAOption"
 	v1 "github.com/einride/protoc-gen-messageintegrity/proto/gen/example/v1"
 	"google.golang.org/protobuf/proto"
 	"log"
+	"os"
+	"path"
 	"testing"
 )
+
+func TestCreatePKCS1(t *testing.T) {
+	tests := []struct {
+		keyID         string
+		length        int
+		expectedError string
+	}{
+		{keyID: "test_id_1", length: 2048, expectedError: ""},
+		{keyID: "test_id_2", length: 4096, expectedError: ""},
+	}
+
+	for _, test := range tests {
+		err := createRSAKeyPair(test.keyID, test.length)
+
+		if err != nil && err.Error() != test.expectedError {
+			t.Errorf("error actual error: %v, expectedError: %v", err, test.expectedError)
+		}
+
+		privKey, err := verificationoptionrsa.FetchPrivateKey(test.keyID)
+		if err != nil {
+			t.Errorf("failed to fetch private key: %v", err)
+		}
+		log.Printf("Case KeyID: %v\n", test.keyID)
+		log.Print(privKey)
+
+		pubKey, err := verificationoptionrsa.FetchPublicKey(test.keyID)
+		if err != nil {
+			t.Errorf("failed to fetch public key: %v", err)
+		}
+		log.Println(pubKey)
+	}
+
+}
+
+func TestParsePKCS1(t *testing.T) {
+	tests := []struct {
+		key_id        string
+		length        int
+		expectedError string
+	}{
+		{key_id: "test_id_1", length: 2048, expectedError: ""},
+		{key_id: "test_id_2", length: 4096, expectedError: ""},
+	}
+
+	for _, test := range tests {
+		err := createRSAKeyPair(test.key_id, test.length)
+
+		if err != nil && err.Error() != test.expectedError {
+			t.Errorf("error actual error: %v, expectedError: %v", err, test.expectedError)
+		}
+	}
+
+}
+
+func createRSAKeyPair(keyID string, length int) error {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+	keysPath := path.Join(home, "integrity-keys")
+	// Generate the RSA Key.
+	privateKey, err := rsa.GenerateKey(rand.Reader, length)
+	if err != nil {
+		return err
+	}
+	publicKey := &privateKey.PublicKey
+
+	// Save the private key to a file.
+	var privateKeyBytes []byte = x509.MarshalPKCS1PrivateKey(privateKey)
+	privateKeyBlock := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: privateKeyBytes,
+	}
+	err = os.MkdirAll(keysPath, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	privatePem, err := os.Create(path.Join(keysPath, fmt.Sprintf("message_integrity_%v_private.pem", keyID)))
+
+	if err != nil {
+		return err
+	}
+	err = pem.Encode(privatePem, privateKeyBlock)
+	if err != nil {
+		return err
+	}
+
+	// dump public key to file
+	publicKeyBytes, err := x509.MarshalPKIXPublicKey(publicKey)
+	if err != nil {
+		return err
+	}
+	publicKeyBlock := &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: publicKeyBytes,
+	}
+	publicPem, err := os.Create(path.Join(keysPath, fmt.Sprintf("message_integrity_%v_public.pem", keyID)))
+	if err != nil {
+		return err
+	}
+	err = pem.Encode(publicPem, publicKeyBlock)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func TestSigningRSA(t *testing.T) {
+	tests := []struct {
+		key                   []byte
+		message               verificationoption.VerifiableMessage
+		expectedSignedMessage verificationoption.VerifiableMessage
+		isValid               bool
+		expectedError         string
+	}{
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerificationOption{SteeringAngle: 5.0}, expectedSignedMessage: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0, Signature: []byte{122, 226, 141, 14, 125, 93, 5, 148, 213, 252, 225, 147, 142, 195, 174, 247, 49, 164, 86, 20, 9, 189, 217, 122, 180, 228, 79, 20, 152, 191, 55, 12}}, isValid: false, expectedError: ""},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0}, expectedSignedMessage: &v1.SteeringCommandVerificationOption{}, isValid: false, expectedError: ""},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerificationOption{}, expectedSignedMessage: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0, Signature: []byte{122, 226, 141, 14, 125, 93, 5, 148, 213, 252, 225, 147, 142, 195, 174, 247, 49, 164, 86, 20, 9, 189, 217, 122, 180, 228, 79, 20, 152, 191, 55, 12}}, isValid: false, expectedError: ""},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0}, expectedSignedMessage: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0, Signature: []byte{5, 151, 155, 117, 81, 80, 154, 220, 0, 88, 88, 194, 100, 6, 74, 66, 99, 251, 28, 141, 118, 114, 87, 140, 120, 207, 59, 210, 133, 179, 150, 107}}, isValid: true, expectedError: ""},
+		{key: nil, message: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0}, expectedSignedMessage: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0, Signature: []byte{5, 151, 155, 117, 81, 80, 154, 220, 0, 88, 88, 194, 100, 6, 74, 66, 99, 251, 28, 141, 118, 114, 87, 140, 120, 207, 59, 210, 133, 179, 150, 107}}, isValid: false, expectedError: "key was nil"},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: nil, expectedSignedMessage: nil, isValid: true, expectedError: "message was nil"},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerification{SteeringAngle: 6.0}, expectedSignedMessage: &v1.SteeringCommandVerification{SteeringAngle: 6.0, Signature: []byte{5, 151, 155, 117, 81, 80, 154, 220, 0, 88, 88, 194, 100, 6, 74, 66, 99, 251, 28, 141, 118, 114, 87, 140, 120, 207, 59, 210, 133, 179, 150, 107}}, isValid: false, expectedError: "failed to find any message integrity signature field in proto"},
+	}
+	for _, test := range tests {
+		log.Printf("Case: %v", test.message)
+		err := verificationoption.SignProto(test.message, test.key)
+		if test.message != nil {
+			log.Printf("Signature: %v", test.message.GetSignature())
+		}
+		if err != nil && err.Error() != test.expectedError {
+			t.Errorf("Error actual = %v, and expected = %v", err, test.expectedError)
+		}
+		if err == nil && test.expectedError != "" {
+			t.Errorf("Error actual = nil, and expected = %v", test.expectedError)
+		}
+		if proto.Equal(test.message, test.expectedSignedMessage) != test.isValid {
+			t.Errorf("error actual signed message: %v expected signed message: %v to be valid: %v", test.message, test.expectedSignedMessage, test.isValid)
+		}
+	}
+}
 
 func TestSigning(t *testing.T) {
 	tests := []struct {
