3474374bfdc59b4be8097682b2f547cb2e3eb748
paulmolloy <molloyp1@tcd.ie>
2021-05-31 17:39:15 +0200

    refactor: copy verificationOption to make verificationRSAOption package

---
+++ internal/verificationOption/verification.go
@@ -0,0 +1,127 @@
+package verificationoption
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	integritypb "github.com/einride/protoc-gen-messageintegrity/proto/gen/integrity/v1"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	descriptorpb "google.golang.org/protobuf/types/descriptorpb"
+	"hash"
+	"log"
+)
+
+const ImplicitMessageIntegrityKey = "IMPLICIT_MESSAGE_INTEGRITY_KEY"
+
+// VerifiableMessage a proto message that has a Signature field.
+// Using the extension interface pattern so that I can read the Signature.
+type VerifiableMessage interface {
+	proto.Message
+	GetSignature() []byte
+	// Can't modify the Signature field since there is no Setters for proto-gen-go fields using protoreflect instead.
+}
+
+// Check the signature embedded in the protobuf message is correct for the message by recalculating
+// it using the secret key.
+func ValidateHMAC(message VerifiableMessage, key []byte) (bool, error) {
+	if message == nil {
+		return false, errors.New("message was nil")
+	}
+	if key == nil {
+		return false, errors.New("key was nil")
+	}
+	signatureFieldDescriptor, err := retrieveSignatureFieldDescriptor(message)
+	if err != nil {
+		return false, err
+	}
+	receivedSig := message.ProtoReflect().Get(signatureFieldDescriptor).Bytes()
+	if receivedSig == nil {
+		return false, errors.New("signature behaviour required but signature not set")
+	}
+	mac := hmac.New(sha256.New, key)
+	// Calculate the expected signature
+	sig, err := calculateSignature(message, mac)
+	if err != nil {
+		return false, err
+	}
+	return bytes.Equal(receivedSig, sig), nil
+}
+
+// SignProto signs a proto that has a proto field Signature.
+func SignProto(message VerifiableMessage, key []byte) error {
+	if message == nil {
+		return errors.New("message was nil")
+	}
+	if key == nil {
+		return errors.New("key was nil")
+	}
+
+	// Using proto reflection to make this work as the field is not otherwise accessible without casting to
+	// a specific type.
+	signatureFieldDescriptor, err := retrieveSignatureFieldDescriptor(message)
+	if err != nil {
+		return err
+	}
+
+	if message.ProtoReflect().Get(signatureFieldDescriptor).Bytes() != nil {
+		log.Printf("Signature for %v has already been set to re-signing...", signatureFieldDescriptor.FullName())
+	}
+	mac := hmac.New(sha256.New, key)
+	sig, err := calculateSignature(message, mac)
+	if err != nil {
+		return err
+	}
+	// Assign the generated signature to the message using reflection as it is a VerifiableMessage and not cast to
+	// its exact proto type.
+	message.ProtoReflect().Set(signatureFieldDescriptor, protoreflect.ValueOfBytes(sig))
+	return nil
+}
+
+// retrieveSignatureFieldDescriptor finds the field where the signature is stored in the message
+func retrieveSignatureFieldDescriptor(message VerifiableMessage) (protoreflect.FieldDescriptor, error) {
+	m := message.ProtoReflect()
+	// Can't use Range() as we can't identify signatures that have not been signed as it skips unset fields.
+	fields := m.Descriptor().Fields()
+	for i := 0; i < fields.Len(); i++ {
+		fd := fields.Get(i)
+		if fd.Kind() != protoreflect.BytesKind {
+			continue // The signature can only be a bytes field, ignore everything else.
+		}
+		opts := fd.Options().(*descriptorpb.FieldOptions)
+		sigOption, ok := proto.GetExtension(opts, integritypb.E_Signature).(*integritypb.Signature)
+		if !ok ||  sigOption.GetBehaviour() == integritypb.SignatureBehaviour_SIGNATURE_BEHAVIOUR_UNSPECIFIED {
+			// Failed to find or cast a MessageIntegrityOption, keep looking.
+			continue
+		}
+		// Only check the first instance of a signature option field.
+		// TODO(paulmolloy): Change to fail if there are multiple signature options in a message.
+		return fd, nil
+	}
+	return nil, errors.New("failed to find any message integrity signature field in proto")
+}
+
+func calculateSignature(message VerifiableMessage, mac hash.Hash) ([]byte, error) {
+	if message == nil {
+		return nil, errors.New("message was nil")
+	}
+	signatureFieldDescriptor, err := retrieveSignatureFieldDescriptor(message)
+	if err != nil {
+		return nil, err
+	}
+
+	// Nil out the sig using reflection.
+	message.ProtoReflect().Clear(signatureFieldDescriptor)
+	// Marshal the message without a signature so that a sig can be generated for it.
+	marshalled, err := proto.Marshal(message)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal proto before signing: %v", err)
+	}
+	if _, err = mac.Write(marshalled); err != nil {
+		return nil, err
+	}
+	// Return the generated signature.
+	return mac.Sum(nil), nil
+}
---
+++ internal/verificationOptionTest/verification_test.go
@@ -0,0 +1,112 @@
+package verificationoptionTest
+
+import (
+	verificationoption "github.com/einride/protoc-gen-messageintegrity/internal/verificationOption"
+	v1 "github.com/einride/protoc-gen-messageintegrity/proto/gen/example/v1"
+	"google.golang.org/protobuf/proto"
+	"log"
+	"testing"
+)
+
+func TestSigning(t *testing.T) {
+	tests := []struct {
+		key                   []byte
+		message               verificationoption.VerifiableMessage
+		expectedSignedMessage verificationoption.VerifiableMessage
+		isValid               bool
+		expectedError         string
+	}{
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerificationOption{SteeringAngle: 5.0}, expectedSignedMessage: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0, Signature: []byte{122, 226, 141, 14, 125, 93, 5, 148, 213, 252, 225, 147, 142, 195, 174, 247, 49, 164, 86, 20, 9, 189, 217, 122, 180, 228, 79, 20, 152, 191, 55, 12}}, isValid: false, expectedError: ""},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0}, expectedSignedMessage: &v1.SteeringCommandVerificationOption{}, isValid: false, expectedError: ""},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerificationOption{}, expectedSignedMessage: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0, Signature: []byte{122, 226, 141, 14, 125, 93, 5, 148, 213, 252, 225, 147, 142, 195, 174, 247, 49, 164, 86, 20, 9, 189, 217, 122, 180, 228, 79, 20, 152, 191, 55, 12}}, isValid: false, expectedError: ""},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0}, expectedSignedMessage: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0, Signature: []byte{5, 151, 155, 117, 81, 80, 154, 220, 0, 88, 88, 194, 100, 6, 74, 66, 99, 251, 28, 141, 118, 114, 87, 140, 120, 207, 59, 210, 133, 179, 150, 107}}, isValid: true, expectedError: ""},
+		{key: nil, message: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0}, expectedSignedMessage: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0, Signature: []byte{5, 151, 155, 117, 81, 80, 154, 220, 0, 88, 88, 194, 100, 6, 74, 66, 99, 251, 28, 141, 118, 114, 87, 140, 120, 207, 59, 210, 133, 179, 150, 107}}, isValid: false, expectedError: "key was nil"},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: nil, expectedSignedMessage: nil, isValid: true, expectedError: "message was nil"},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerification{SteeringAngle: 6.0}, expectedSignedMessage: &v1.SteeringCommandVerification{SteeringAngle: 6.0, Signature: []byte{5, 151, 155, 117, 81, 80, 154, 220, 0, 88, 88, 194, 100, 6, 74, 66, 99, 251, 28, 141, 118, 114, 87, 140, 120, 207, 59, 210, 133, 179, 150, 107}}, isValid: false, expectedError: "failed to find any message integrity signature field in proto"},
+	}
+	for _, test := range tests {
+		log.Printf("Case: %v", test.message)
+		err := verificationoption.SignProto(test.message, test.key)
+		if test.message != nil {
+			log.Printf("Signature: %v", test.message.GetSignature())
+		}
+		if err != nil && err.Error() != test.expectedError {
+			t.Errorf("Error actual = %v, and expected = %v", err, test.expectedError)
+		}
+		if err == nil && test.expectedError != "" {
+			t.Errorf("Error actual = nil, and expected = %v", test.expectedError)
+		}
+		if proto.Equal(test.message, test.expectedSignedMessage) != test.isValid {
+			t.Errorf("error actual signed message: %v expected signed message: %v to be valid: %v", test.message, test.expectedSignedMessage, test.isValid)
+		}
+	}
+}
+
+func TestSignatureVerification(t *testing.T) {
+	tests := []struct {
+		key           []byte
+		message       verificationoption.VerifiableMessage
+		expectedValue bool
+		expectedError string
+	}{
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerificationOption{SteeringAngle: 5.0}, expectedValue: true, expectedError: ""},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0}, expectedValue: true, expectedError: ""},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerificationOption{}, expectedValue: true, expectedError: ""},
+		{key: nil, message: &v1.SteeringCommandVerificationOption{}, expectedValue: false, expectedError: "key was nil"},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: nil, expectedValue: false, expectedError: "message was nil"},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerification{SteeringAngle: 6.0}, expectedValue: false, expectedError: "failed to find any message integrity signature field in proto"},
+	}
+	for _, test := range tests {
+		log.Printf("Case: %v", test.message)
+		// Don't check error as we want to test robustness of ValidateHMAC.
+		_ = verificationoption.SignProto(test.message, test.key)
+		if test.message != nil {
+			log.Printf("Signature: %v", test.message.GetSignature())
+		}
+		isValid, err := verificationoption.ValidateHMAC(test.message, test.key)
+		if err != nil && err.Error() != test.expectedError {
+			t.Errorf("Error actual = %v, and expected = %v", err, test.expectedError)
+		}
+		if err == nil && test.expectedError != "" {
+			t.Errorf("Error actual = nil, and expected = %v", test.expectedError)
+		}
+		if isValid != test.expectedValue {
+			t.Errorf("isValid actual = %v , and expected = %v", isValid, test.expectedValue)
+		}
+	}
+}
+
+func TestSignatureVerificationModification(t *testing.T) {
+	tests := []struct {
+		key             []byte
+		message         verificationoption.VerifiableMessage
+		receivedMessage verificationoption.VerifiableMessage
+		expectedValue   bool
+		expectedError   string
+	}{
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerificationOption{SteeringAngle: 5.0}, receivedMessage: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0, Signature: []byte{122, 226, 141, 14, 125, 93, 5, 148, 213, 252, 225, 147, 142, 195, 174, 247, 49, 164, 86, 20, 9, 189, 217, 122, 180, 228, 79, 20, 152, 191, 55, 12}}, expectedValue: false, expectedError: ""},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0}, receivedMessage: &v1.SteeringCommandVerificationOption{}, expectedValue: false, expectedError: "signature behaviour required but signature not set"},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerificationOption{}, receivedMessage: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0, Signature: []byte{122, 226, 141, 14, 125, 93, 5, 148, 213, 252, 225, 147, 142, 195, 174, 247, 49, 164, 86, 20, 9, 189, 217, 122, 180, 228, 79, 20, 152, 191, 55, 12}}, expectedValue: false, expectedError: ""},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0}, receivedMessage: &v1.SteeringCommandVerificationOption{SteeringAngle: 6.0, Signature: []byte{5, 151, 155, 117, 81, 80, 154, 220, 0, 88, 88, 194, 100, 6, 74, 66, 99, 251, 28, 141, 118, 114, 87, 140, 120, 207, 59, 210, 133, 179, 150, 107}}, expectedValue: true, expectedError: ""},
+		{key: []byte{253, 131, 172, 77, 207, 188, 17, 98, 0, 235, 48, 62, 175, 191, 75, 36, 181, 119, 22, 36, 95, 180, 254, 180, 180, 14, 39, 255, 104, 211, 146, 113}, message: &v1.SteeringCommandVerification{SteeringAngle: 6.0}, receivedMessage: &v1.SteeringCommandVerification{SteeringAngle: 6.0, Signature: []byte{5, 151, 155, 117, 81, 80, 154, 220, 0, 88, 88, 194, 100, 6, 74, 66, 99, 251, 28, 141, 118, 114, 87, 140, 120, 207, 59, 210, 133, 179, 150, 107}}, expectedValue: false, expectedError: "failed to find any message integrity signature field in proto"},
+	}
+	for _, test := range tests {
+		log.Printf("Case: %v", test.message)
+		// Don't check error as we want to test robustness of ValidateHMAC.
+		_ = verificationoption.SignProto(test.message, test.key)
+		if test.receivedMessage != nil {
+			log.Printf("Signature of Received Message: %v", test.receivedMessage.GetSignature())
+		}
+		isValid, err := verificationoption.ValidateHMAC(test.receivedMessage, test.key)
+		if err != nil && err.Error() != test.expectedError {
+			t.Errorf("Error actual = %v, and expected = %v", err, test.expectedError)
+		}
+		if err == nil && test.expectedError != "" {
+			t.Errorf("Error actual = nil, and expected = %v", test.expectedError)
+		}
+		if isValid != test.expectedValue {
+			t.Errorf("Error actual isValid: %v, expectedValue: %v for message: %v receivedMessge: %v", isValid, test.expectedValue, test.message, test.receivedMessage)
+		}
+	}
+
+}
